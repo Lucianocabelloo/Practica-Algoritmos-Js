4fa0aa28973327fbee22b6a9c02418db65130758
lucianocabelloo <you@example.com>
2023-10-30 23:44:34 -0300

    Añadido ejercicio 10

--- practico1-VariablesCondicionales/js/bucles.js
+++ practico1-VariablesCondicionales/js/bucles.js
@@ -65,22 +65,7 @@
 // 10
 
 // Dificultad:  🟢🟡🔴
-// 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 
-// let filas = parseInt(prompt("Ingrese la cantidad de filas"))
-// let columnas = parseInt(prompt("Ingrese la cantidad de columnas"))
-// let celdas = filas * columnas
-// document.write(`<table> <tbody>`)
-
-// for(let indiceFilas = 1; indiceFilas <= filas; indiceFilas++){
-
-//     document.write(`<tr>`);
-//     for(let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas++){
-//         document.write(`<td>${celdas--}</td>`);
-//     }
-//     document.write(`</tr>`);
-// }
-// document.write(`</tbody></table>`)
 
 // Dificultad:  🟢🟡
 // Ejercicios con Math
