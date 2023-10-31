1cb438256a3873b693d4160b299d61219ea2dcd3
lucianocabelloo <you@example.com>
2023-10-30 23:49:03 -0300

    Ejercicio 13 añadido

--- practico1-VariablesCondicionales/js/bucles.js
+++ practico1-VariablesCondicionales/js/bucles.js
@@ -80,11 +80,7 @@
 // Ejercicios con String
 
 // Dificultad:  🟢🟡
-// 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
 
-// let solicitarTexto = prompt("Ingrese un texto para mostrarlo en mayusculas")
-// let textoEnMayusculas = solicitarTexto.toUpperCase()
-// document.write(`<p>${textoEnMayusculas}</p>`)
 
 
 // Dificultad:  🟢🟡
---
+++ practico1-VariablesCondicionales/js/bucles/ejercicio13.js
@@ -0,0 +1,5 @@
+// 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+ let solicitarTexto = prompt("Ingrese un texto para mostrarlo en mayusculas")
+ let textoEnMayusculas = solicitarTexto.toUpperCase()
+ document.write(`<p>${textoEnMayusculas}</p>`)
