af6a3810c1c48ec04969d23dd9ad0e71dac8632f
lucianocabelloo <you@example.com>
2023-10-30 23:50:59 -0300

    ejercicio 14 añadido

--- practico1-VariablesCondicionales/js/bucles.js
+++ practico1-VariablesCondicionales/js/bucles.js
@@ -84,11 +84,7 @@
 
 
 // Dificultad:  🟢🟡
-// 14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
-// let nombre = "luciano"
-// let textoSeparado = solicitarTexto.split("").join("-")
 
-// document.write(`${textoSeparado}`)
 // console.log(textoSeparado)
 
 
