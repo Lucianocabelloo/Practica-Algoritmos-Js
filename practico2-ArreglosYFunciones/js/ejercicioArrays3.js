95f6ed8d1766ce53b5aee2903dca5d44191923d6
lucianocabelloo <you@example.com>
2023-11-01 23:51:14 -0300

    Añadido ejercicio 3

---
+++ practico2-ArreglosYFunciones/js/ejercicioArrays3.js
@@ -0,0 +1,20 @@
+// 3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
+
+// Ejemplo de salida:
+const apariciones = [];
+
+
+for (let i = 0; i <= 12; i++) {
+    apariciones.push(0);
+}
+
+for (let indiceDeAparicion = 0; indiceDeAparicion < 50; indiceDeAparicion++) {
+    let numeroRandom1 = Math.floor(Math.random() * 6) + 1; 
+    let numeroRandom2 = Math.floor(Math.random() * 6) + 1; 
+    let sumaNumerosRandom = numeroRandom1 + numeroRandom2; 
+
+    apariciones[sumaNumerosRandom]++; 
+}
+for (let i = 2; i <= 12; i++) {
+    document.write(`<p>El número ${i} se repitió ${apariciones[i]} veces</p>`);
+}
