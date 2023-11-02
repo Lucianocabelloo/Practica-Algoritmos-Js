35099fe687feeeb5413f5a7967068e8411f92078
lucianocabelloo <you@example.com>
2023-11-02 09:16:19 -0300

    Poner consignas en sus respectivos ejercicios

--- practico2-ArreglosYFunciones/js/ejercicioArrays4.js
+++ practico2-ArreglosYFunciones/js/ejercicioArrays4.js
@@ -1,3 +1,6 @@
+// 4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+
+
 function numeroParoImpar(numero) {
     
     if(numero % 2 === 0){
--- practico2-ArreglosYFunciones/js/practico2.js
+++ practico2-ArreglosYFunciones/js/practico2.js
@@ -31,12 +31,12 @@
 
 // Funciones
 // Dificultad:  🟢
-// 4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+
 
 
 
 // Dificultad:  🟢🟡
-// 5- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
 
 
 // Dificultad:  🟢🟡
