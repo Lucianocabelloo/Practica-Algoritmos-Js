9a70a02e407eb0acb3bace8d706fa1326e6996bd
lucianocabelloo <you@example.com>
2023-11-08 23:20:36 -0300

    Ejercicio 1 metodos de arrays finalizado

--- practico2-ArreglosYFunciones/js/ejerciciosMetodosArrays/ejercicios.js
+++ practico2-ArreglosYFunciones/js/ejerciciosMetodosArrays/ejercicios.js
@@ -5,7 +5,6 @@
 // Gestión de inventario en una tienda: Tienes dos arrays, uno que representa los productos en stock y otro que almacena los precios de los productos. Debes crear funciones que permitan agregar nuevos productos al inventario, eliminar productos agotados y calcular el valor total del inventario.
 
 
-
 // Registro de notas de estudiantes: Tienes dos arrays, uno con nombres de estudiantes y otro con sus respectivas notas en un curso. Debes crear funciones que calculen el promedio de notas por estudiante y muestren un resumen con los nombres de los estudiantes que aprobaron y reprobaron el curso.
 
 // Catálogo de biblioteca: Tienes un array que almacena títulos de libros en una biblioteca. Debes crear funciones que permitan a los usuarios buscar libros por título, verificar si un libro específico está disponible para préstamo y llevar un registro de la disponibilidad de los libros.
---
+++ practico2-ArreglosYFunciones/js/ejerciciosMetodosArrays/metodosArrays1.js
@@ -0,0 +1,89 @@
+// Gestión de inventario en una tienda: Tienes dos arrays, uno que representa los productos en stock y otro que almacena los precios de los productos. Debes crear funciones que permitan agregar nuevos productos al inventario, eliminar productos agotados y calcular el valor total del inventario.
+
+const productosEnStock = [
+  "Camiseta",
+  "Pantalón",
+  "Zapatos",
+  "Sombrero",
+  "Bufanda",
+  "Campera",
+];
+const preciosProductos = [15.99, 29.99, 49.99, 9.99, 12.5, "Sin Stock"];
+console.log(productosEnStock);
+
+/* Funcion Añadir Producto */
+
+function agregarProducto(productoAñadido, precioProductoAñadido) {
+  if (productoAñadido !== undefined && precioProductoAñadido !== undefined) {
+    productosEnStock.push(productoAñadido);
+    preciosProductos.push(precioProductoAñadido);
+    console.log(productosEnStock, preciosProductos);
+  } else {
+    document.write(
+      `<p>Por favor añada los productos y su precio correspondiente</p>`
+    );
+  }
+}
+
+/* Funcion Eliminar Producto */
+
+function eliminarProducto() {
+  const indicesProductosSinStock = [];
+
+  for (let i = 0; i < preciosProductos.length; i++) {
+    if (preciosProductos[i] === "Sin Stock") {
+      indicesProductosSinStock.push(i);
+    }
+  }
+
+  for (let i = indicesProductosSinStock.length - 1; i >= 0; i--) {
+    const indice = indicesProductosSinStock[i];
+    productosEnStock.splice(indice, 1);
+    preciosProductos.splice(indice, 1);
+  }
+}
+
+document.write("<ul><h2> Listado de productos original</h2>");
+
+for (let i = 0; i < productosEnStock.length; i++) {
+  document.write(
+    `<li>${productosEnStock[i]} - Precio: ${preciosProductos[i]}</li>`
+  );
+}
+document.write("</ul>");
+/* Llamada para Añadir Producto */
+agregarProducto("Guantes", 5.99);
+
+
+document.write("<ul><h2> Listado de productos con agregado</h2>");
+
+for (let i = 0; i < productosEnStock.length; i++) {
+  document.write(
+    `<li>${productosEnStock[i]} - Precio: ${preciosProductos[i]}</li>`
+  );
+}
+document.write("</ul>");
+
+/* Llamada para elinminar Producto */
+eliminarProducto();
+
+document.write("<ul><h2> Listado de productos sin stock eliminado</h2>");
+
+for (let i = 0; i < productosEnStock.length; i++) {
+  document.write(
+    `<li>${productosEnStock[i]} - Precio: ${preciosProductos[i]}</li>`
+  );
+}
+document.write("</ul>");
+
+/* Funcion Sumar precios del stock Producto */
+
+function calcularValorTotal() {
+    const valorInicial = 0
+    const sumatoriaDePrecios = preciosProductos.reduce((acc, valorActual) => acc + valorActual, valorInicial)
+    const sumatoriaRedondeada = Math.round(sumatoriaDePrecios)
+    document.write(`<h3> La sumatoria del precio de todos los productos es : ${sumatoriaRedondeada} </h3>`)
+}
+
+
+calcularValorTotal()
