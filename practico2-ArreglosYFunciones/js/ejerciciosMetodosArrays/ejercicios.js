e8f5b9ec510e3d481ce3ab60cac7e3543d4159e5
lucianocabelloo <you@example.com>
2023-11-08 22:33:35 -0300

    Inicio ejercicios de metodos de arrays y reestructuracion de carpeta js

---
+++ practico2-ArreglosYFunciones/js/ejerciciosMetodosArrays/ejercicios.js
@@ -0,0 +1,13 @@
+// Completar los quiz del campus sobre js
+
+// realizar los siguientes ejercicios para practicar métodos de arreglos:
+
+// Gestión de inventario en una tienda: Tienes dos arrays, uno que representa los productos en stock y otro que almacena los precios de los productos. Debes crear funciones que permitan agregar nuevos productos al inventario, eliminar productos agotados y calcular el valor total del inventario.
+
+
+
+// Registro de notas de estudiantes: Tienes dos arrays, uno con nombres de estudiantes y otro con sus respectivas notas en un curso. Debes crear funciones que calculen el promedio de notas por estudiante y muestren un resumen con los nombres de los estudiantes que aprobaron y reprobaron el curso.
+
+// Catálogo de biblioteca: Tienes un array que almacena títulos de libros en una biblioteca. Debes crear funciones que permitan a los usuarios buscar libros por título, verificar si un libro específico está disponible para préstamo y llevar un registro de la disponibilidad de los libros.
+
+// Gestión de proyectos en una empresa: Tienes dos arrays, uno para los nombres de proyectos y otro para los estados de los proyectos. Debes crear funciones que permitan agregar nuevos proyectos, cambiar el estado de un proyecto, asignar un responsable y generar un resumen de proyectos en curso.
---
+++ practico2-ArreglosYFunciones/practicaArrays.js
@@ -0,0 +1,31 @@
+let productos = [
+    "Hidratante facial",
+    "Tonico exfoliante",
+    "Protector solar factor 50",
+    "Protector solar factor 50",
+    "Protector solar factor 50",
+    "Protector solar toque seco factor 50",
+    "Protector solar con color factor 50",
+    "Gel de limpieza facial",
+    "Tonico hidratante",
+    "Sérum hidratante",
+    "Exfoliante de azúcar cherry",
+    "Pads de hidrogel para contorno de ojos",
+    "Mascarilla facial",
+    "Sérum facial con PHA",
+    "Agua micellar",
+    "Mascara de hidratación y reparación",
+    "Protector solar en barra",
+    "Crema contorno de ojos",
+    "Bálsamo labial",
+  ];
+
+
+
+  const protectoresSolares = productos.filter(products => products == "Protector solar factor 50")
+  const todosLosProtectoresSolares = productos.filter(products => products.includes("Protector"))
+console.log(todosLosProtectoresSolares)
+
+const buscarProducto2 = productos.find(products => products.includes("Hidratante"))
+
+document.write(`<p>El producto buscado ${buscarProducto2 !== undefined ? buscarProducto2 : "No se encontro el producto buscado"}`)
